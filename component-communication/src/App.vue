cf8dabdc6fc31489d8dabc926f8e6f5b348fdef5
vishnugt <vishnugt@hotmail.com>
2025-02-22 15:25:19 +0530

    Add component communication

---
+++ component-communication/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h2>My Friends</h2>
+  <ul>
+    <friend-contact v-for="friend in friends" :key="friend.id" :name="friend.name" :email-address="friend.email"
+                    :phone-number="friend.phone"></friend-contact>
+  </ul>
+</template>
+
+<script>
+
+const app = {
+  data() {
+    return {
+      friends: [{
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'abc@gmail.com'
+      }, {
+        id: 'julie',
+        name: 'Julie Jones',
+        phone: '09876 543 221',
+        email: 'abc@hotmail.com'
+      }]
+    }
+  }
+}
+
+export default app;
+</script>
+
+
---
+++ component-communication/src/components/FriendContact.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  //This also works
+  /*  props: [
+      'name',
+      'phoneNumber',
+      'emailAddress'
+    ],*/
+  props: {
+    name: String,
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress: {
+      type: String,
+      required: false,
+      default: function () {
+        return "redacted@gmail.com"
+      },
+      validator: function (value) {
+        console.log(value)
+        return value.includes(".com")
+      }
+    }
+  },
+  data() {
+    return {
+      showDetails: false
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.showDetails = !this.showDetails;
+    }
+  }
+}
+</script>
+
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails"> {{ !showDetails ? "Show Details" : "Hide Details" }}</button>
+    <ul v-if="showDetails">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<style>
+
+</style>
